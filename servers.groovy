920238d2be2d610fb2cf7b36c9cd6e614320cfbe
Lionel <lionelve@gmail.com>
2016-07-13 06:22:22 +0000

    no need for sudo apparently

--- servers.groovy
+++ servers.groovy
@@ -1,17 +1,17 @@
 def deploy(id, port, ver) {
- sh "sudo docker inspect -f {{.State.Running}} ${id} > RUNNING"
+ sh "docker inspect -f {{.State.Running}} ${id} > RUNNING"
  isRunning=readFile('RUNNING')
  if(isRunning == 'true') {
      undeploy id
  }
  echo "Running image tag ${ver} as ${id}"
- sh "sudo docker run --name ${id} -d -p ${port}:8080 lionelve/demo-war:${ver}"
+ sh "docker run --name ${id} -d -p ${port}:8080 lionelve/demo-war:${ver}"
 }
 
 def undeploy(id) {
     echo "undeploying ${id}"
-    sh "sudo docker stop ${id}"
-    sh "sudo docker rm ${id}"
+    sh "docker stop ${id}"
+    sh "docker rm ${id}"
 }
 
 
